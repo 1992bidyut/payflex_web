7f898fcdd05525e3f8cc27c96d9bfe74305d9f15
Mohsin <wing_field@outlook.com>
2020-08-18 21:53:33 +0600

    method name fixed

--- application/models/Payment_Model.php
+++ application/models/Payment_Model.php
@@ -3,7 +3,31 @@
 class Payment_Model extends CI_Model
 {
     public function getpaymentdetail($order_code){
-        $this->db->select('*')
+        $this->db->select('tbl_payment.id as payment_id, 
+        tbl_payment.trxid,
+        tbl_payment.payment_mode_id,
+        tbl_payment.financial_institution_id,
+        tbl_payment.payment_date_time,
+        tbl_payment.reference_no,
+        tbl_payment.order_code,
+        tbl_payment.amount,
+        tbl_payment.order_id,
+        tbl_payment.action_flag,
+        tbl_payment.submitted_date,
+        tbl_payment_image_relation.id as payment_image_relation_id,
+        tbl_payment_image_relation.image_id,
+        tbl_payment_image_relation.user_id,
+        tbl_payment_image_relation.payment_id,
+        tbl_image.id,
+        tbl_image.trxid,
+        tbl_image.image_name,
+        tbl_image.user_id,
+        tbl_image.request_time,
+        tbl_image.upload_time,
+        tbl_image.image_discription,
+        tbl_image.image_type_id,
+        tbl_image.purpose,
+        ')
             ->from('tbl_payment')
             ->join('tbl_payment_image_relation','tbl_payment_image_relation.payment_id = tbl_payment.id','left')
             ->join('tbl_image','tbl_image.id = tbl_payment_image_relation.image_id')
