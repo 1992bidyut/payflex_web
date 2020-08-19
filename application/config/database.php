a84107b1f442f3d578f89eccd506b2866f8c3b39
Mohsin <wing_field@outlook.com>
2020-08-19 16:07:39 +0600

    working on update

--- application/config/database.php
+++ application/config/database.php
@@ -72,7 +72,7 @@ $active_record = TRUE;
 //TODO: chnage database info
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "";
+$db['default']['password'] = "root";
 $db['default']['database'] = "payflex_1v1";
 $db['default']['dbdriver'] = "mysqli";
 $db['default']['dbprefix'] = "";
