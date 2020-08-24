17b1e625ba019daa2a70987273e700f2fca2b70c
Bidyut Debnath <1992bidyut@gmail.com>
2020-08-25 00:13:57 +0600

    printing page fix

--- application/models/DashboardsModel.php
+++ application/models/DashboardsModel.php
@@ -5,7 +5,7 @@ class DashboardsModel extends CI_Model
     public function orderCounts($startingDate, $endingDate)
     {
         $myQueryString ="SELECT count(id) as number_of_order FROM tbl_customer_order 
-         WHERE delivery_date b>='" . $startingDate . "') and delivery_date <='". $endingDate ."')";
+         WHERE delivery_date >='" . $startingDate . "') and delivery_date <='". $endingDate ."')";
          //die ($myQueryString); 
         if( empty($startingDate) || empty($endingDate))
         {
