5060732e24244f9c735cf64fd86b88938c61e998
Bidyut Debnath <1992bidyut@gmail.com>
2020-09-13 18:52:21 +0600

    leaderboard data export, but not formated, finance report panel and data export

--- application/views/leader/leader.php
+++ application/views/leader/leader.php
@@ -120,7 +120,7 @@
                                             $localImgageBasePath2 = "http://localhost/asset/images/";
                                             $remorteImageBasePath = "http://demo.onuserver.com/payFlex/asset/images/";
                                             $imageName = $data['image_name'];
-                                            $imagePath = $localImgageBasePath . $data['clientId'] . "/";
+                                            $imagePath = $remorteImageBasePath . $data['clientId'] . "/";
                                             $imagePath .= $imageName;
                                             echo '<img style="width: 100%; hight:10px;" src="' . $imagePath . '" alt="' . $imageName . '">';
                                         } else {
